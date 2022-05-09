f0d4992832edae23486375b7311987aa25a9c3e0
Liuhao Wu <liuhaowu@uchicago.edu>
2022-05-08 21:14:05 -0500

    ar tag working

--- scripts/actions.py
+++ scripts/actions.py
@@ -57,11 +57,17 @@ class Actions(object):
 
         # a list of opitimized action numbers
         self.opt_actions = []
-        self.distance = 0.2
+        self.distance = 0.4
         self.curr_target = 2
-        self.curr_tag = 3
+        self.curr_tag = 1
         self.get_action()
         self.front_dist = 5
+
+        self.detect = 1 # 0 = color, 1 = AR tag
+
+        # load DICT_4X4_50
+        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+
 
 
 
@@ -91,91 +97,153 @@ class Actions(object):
     
     def image_callback(self, msg):
 
-        # converts the incoming ROS message to OpenCV format and HSV (hue, saturation, value)
-        image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-        #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        # TODO: define the upper and lower bounds for what should be considered 'yellow'
-        # Note: see class page for hints on this
-        #lower_yellow = np.array([10, 80, 80]) #TODO
-        #upper_yellow = np.array([50, 240, 240]) #TODO
-        
-
-        if self.curr_target == 0:
-            #pink RGB - 254, 4, 153
-            lower_color = (100, 0, 80) #(100, 0, 80)
-            upper_color = (255, 90, 230) #(255,100,230)
+        if self.detect == 0: # detecting color
+            self.distance = 0.2
+            # converts the incoming ROS message to OpenCV format and HSV (hue, saturation, value)
+            image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
             
-            
-        elif self.curr_target ==1:
-            #green RGB #TODO
-            lower_color = np.array([0,75,110])
-            upper_color = np.array([90,255,255])
-        else:
-            #blue RGB #TODO
-            lower_color = np.array([115,90,0])
-            upper_color = np.array([235,255,100])
+            # TODO: define the upper and lower bounds for what should be considered 'yellow'
+            # Note: see class page for hints on this
+            #lower_yellow = np.array([10, 80, 80]) #TODO
+            #upper_yellow = np.array([50, 240, 240]) #TODO
             
 
-        #RGB
-        #Pink - 254, 4, 153
-        #Green - 169, 207, 0
-        #Blue - 0, 192, 228
-
-
-        
-        # this erases all pixels that aren't yellow
-        mask = cv2.inRange(image, lower_color, upper_color)
-
-        # this limits our search scope to only view a slice of the image near the ground
-        h, w, d = image.shape
-        # search_top = int(3*h/4)
-        # search_bot = int(3*h/4 + 20)
-        # mask[0:search_top, 0:w] = 0
-        # mask[search_bot:h, 0:w] = 0
-
-        # using moments() function, the center of the yellow pixels is determined
-        M = cv2.moments(mask)
-
-        my_twist = Twist()
-
-        # if there are any yellow pixels found
-        if M['m00'] > 0:
-            # center of the yellow pixels in the image
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-
-            # a red circle is visualized in the debugging window to indicate
-            # the center point of the yellow pixels
-            # hint: if you don't see a red circle, check your bounds for what is considered 'yellow'
-            cv2.circle(image, (cx, cy), 10, (0,0,255), -1)
-
-            # TODO: based on the location of the line (approximated
-            #       by the center of the yellow pixels), implement
-            #       proportional control to have the robot follow
-            #       the yellow line
-            k_a = 5
-            k_l = -0.1
-            e_a = (w/2 - cx)/w
-            if (abs(e_a) > 0.01):
-                my_twist.angular.z = k_a * e_a
+            if self.curr_target == 0:
+                #pink RGB - 254, 4, 153
+                lower_color = (100, 0, 80) #(100, 0, 80)
+                upper_color = (255, 90, 230) #(255,100,230)
+                
+                
+            elif self.curr_target ==1:
+                #green RGB #TODO
+                lower_color = np.array([0,75,110])
+                upper_color = np.array([90,255,255])
             else:
-                my_twist.angular.z = 0
+                #blue RGB #TODO
+                lower_color = np.array([115,90,0])
+                upper_color = np.array([235,255,100])
+                
+
+            #RGB
+            #Pink - 254, 4, 153
+            #Green - 169, 207, 0
+            #Blue - 0, 192, 228
+
+
             
-            if self.front_dist == 0:
-                my_twist.linear.x = 0.1
-            else:
-                dx = abs(self.distance - self.front_dist)
-                if dx < 0.1:
-                    my_twist.linear.x = 0
+            # this erases all pixels that aren't yellow
+            mask = cv2.inRange(image, lower_color, upper_color)
+
+            # this limits our search scope to only view a slice of the image near the ground
+            h, w, d = image.shape
+            # search_top = int(3*h/4)
+            # search_bot = int(3*h/4 + 20)
+            # mask[0:search_top, 0:w] = 0
+            # mask[search_bot:h, 0:w] = 0
+
+            # using moments() function, the center of the yellow pixels is determined
+            M = cv2.moments(mask)
+
+            my_twist = Twist()
+
+            # if there are any yellow pixels found
+            if M['m00'] > 0:
+                # center of the yellow pixels in the image
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+
+                # a red circle is visualized in the debugging window to indicate
+                # the center point of the yellow pixels
+                # hint: if you don't see a red circle, check your bounds for what is considered 'yellow'
+                cv2.circle(image, (cx, cy), 10, (0,0,255), -1)
+
+                # TODO: based on the location of the line (approximated
+                #       by the center of the yellow pixels), implement
+                #       proportional control to have the robot follow
+                #       the yellow line
+                k_a = 5
+                k_l = -0.1
+                e_a = (w/2 - cx)/w
+                if (abs(e_a) > 0.01):
+                    my_twist.angular.z = k_a * e_a
                 else:
-                    e_l = (self.distance - self.front_dist)/self.front_dist
-                    print("e_l:",e_l)
-                    my_twist.linear.x = k_l * e_l
-            print("linear x", my_twist.linear.x )
-        
-        else:
-            my_twist.angular.z = 0.5
+                    my_twist.angular.z = 0
+                
+                if self.front_dist == 0:
+                    my_twist.linear.x = 0.1
+                else:
+                    dx = abs(self.distance - self.front_dist)
+                    if dx < 0.1:
+                        my_twist.linear.x = 0
+                    else:
+                        e_l = (self.distance - self.front_dist)/self.front_dist
+                        print("e_l:",e_l)
+                        my_twist.linear.x = k_l * e_l
+                print("linear x", my_twist.linear.x )
+            
+            else:
+                my_twist.angular.z = 0.5
+        else: # detecting AR tag
+            self.distance = 0.4
+            image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+            grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            h, w, d = image.shape
+            # search for tags from DICT_4X4_50 in a GRAYSCALE image
+            corners, ids, rejected_points = cv2.aruco.detectMarkers(grayscale_image, self.aruco_dict)
+            
+            my_twist = Twist()
+
+            tot_x = 0
+            tot_y = 0
+            cx = 0
+            cy = 0
+            tag_found = False
+            if ids is not None:
+                for i in range(len(ids)):
+                    if ids[i][0] == self.curr_tag:
+                        for j in range(4):
+                            tot_x += corners[i][0][j][0] #x
+                            tot_y += corners[i][0][j][1] #y
+                        cx = int(tot_x / 4)
+                        cy = int(tot_y / 4)
+                        tag_found = True
+            
+
+            # if there are any yellow pixels found
+            if tag_found:
+
+                # a red circle is visualized in the debugging window to indicate
+                # the center point of the yellow pixels
+                # hint: if you don't see a red circle, check your bounds for what is considered 'yellow'
+                cv2.circle(image, (cx, cy), 10, (0,0,255), -1)
+
+                # TODO: based on the location of the line (approximated
+                #       by the center of the yellow pixels), implement
+                #       proportional control to have the robot follow
+                #       the yellow line
+                k_a = 5
+                k_l = -0.1
+                e_a = (w/2 - cx)/w
+                if (abs(e_a) > 0.01):
+                    my_twist.angular.z = k_a * e_a
+                else:
+                    my_twist.angular.z = 0
+                
+                if self.front_dist == 0:
+                    my_twist.linear.x = 0.1
+                else:
+                    dx = abs(self.distance - self.front_dist)
+                    if dx < 0.1:
+                        my_twist.linear.x = 0
+                    else:
+                        e_l = (self.distance - self.front_dist)/self.front_dist
+                        print("e_l:",e_l)
+                        my_twist.linear.x = k_l * e_l
+                print("linear x", my_twist.linear.x )
+            
+            else:
+                my_twist.angular.z = 0.5
 
         self.cmd_pub.publish(my_twist)
 
